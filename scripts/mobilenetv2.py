841a57dc44299514567a3e71a6bda27da9aa6b57
gokulkrishna98 <gokulkrishna98@gmail.com>
2024-08-04 19:32:56 -0700

    [Script] Fixed MobileNet.py Trace Error -> Fixed the Tensor's not like error by making model in eval model, reasone explained here:
    "I had the same error, but then I realized I had not called model.eval().
     If you don't set your model to evaluate mode, it will still use things
    like drop out which will add randomness to your model making the results not match."
    From : https://github.com/pytorch/pytorch/issues/23999#issuecomment-581687224
    
    Signed-off-by: gokulkrishna98 <gokulkrishna98@gmail.com>

--- scripts/mobilenetv2.py
+++ scripts/mobilenetv2.py
@@ -1,9 +1,9 @@
 import torch
 
 model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
+model.eval()
 
 input = torch.rand(1, 3, 224, 224)
 traced_script_module = torch.jit.trace(model, input)
 
 traced_script_module.save("../models/mobilenetv2.pt")
-    
