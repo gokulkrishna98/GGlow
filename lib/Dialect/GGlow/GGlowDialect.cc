e136f00b6949e3b01826210f2c13fc23edd8a1e9
gokulkrishna98 <gokulkrishna98@gmail.com>
2024-08-06 19:13:09 -0700

    Clang Formated the Dialect files

--- lib/Dialect/GGlow/GGlowDialect.cc
+++ lib/Dialect/GGlow/GGlowDialect.cc
@@ -1,16 +1,14 @@
-#include "llvm/include/llvm/ADT/TypeSwitch.h"
 #include "mlir/include/mlir/IR/Builders.h"
+#include "llvm/include/llvm/ADT/TypeSwitch.h"
 
-#include "lib/Dialect/GGlow/GGlowDialect.h"
 #include "lib/Dialect/GGlow/GGlowDialect.cpp.inc"
+#include "lib/Dialect/GGlow/GGlowDialect.h"
 
 namespace mlir::gglow {
 
-void GlowDialect::initialize(){
+void GlowDialect::initialize()
+{
     // add types and operations
-    // done
-    int a = 1+2;
-    (void)a;
     return;
 }
 
--- lib/Dialect/GGlow/GGlowDialect.h
+++ lib/Dialect/GGlow/GGlowDialect.h
@@ -1,8 +1,7 @@
 #ifndef MLIR_DIALECT_GGLOW_GGLOWDIALECT_H
 #define MLIR_DIALECT_GGLOW_GGLOWDIALECT_H
 
-#include "mlir/include/mlir/IR/DialectImplementation.h"
 #include "lib/Dialect/GGlow/GGlowDialect.h.inc"
+#include "mlir/include/mlir/IR/DialectImplementation.h"
 
 #endif
-
