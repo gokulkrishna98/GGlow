47d538b7169be6e667c5d0a1c1ea54ef4c61e809
gokulkrishna98 <gokulkrishna98@gmail.com>
2024-08-07 14:15:28 -0700

    [GlowDialect] Fixed Header file order, resulting in dependency issue - > Fixes the error unable to file gglow namespae from mlir, due to header file order issue
    Signed-off-by: gokulkrishna98 <gokulkrishna98@gmail.com>

--- lib/Dialect/GGlow/GGlowDialect.cc
+++ lib/Dialect/GGlow/GGlowDialect.cc
@@ -1,8 +1,9 @@
 #include "mlir/include/mlir/IR/Builders.h"
 #include "llvm/include/llvm/ADT/TypeSwitch.h"
 
-#include "lib/Dialect/GGlow/GGlowDialect.cpp.inc"
 #include "lib/Dialect/GGlow/GGlowDialect.h"
+
+#include "lib/Dialect/GGlow/GGlowDialect.cpp.inc"
 
 namespace mlir::gglow {
 
