c94b854f6ede007db74e263f2779b0124872256f
gokulkrishna98 <gokulkrishna98@gmail.com>
2025-01-15 01:13:52 -0500

    [gglow] changed test ir to check validity.

--- lib/Dialect/GGlow/GGlow.cc
+++ lib/Dialect/GGlow/GGlow.cc
@@ -19,8 +19,8 @@ void test_GGlow(){
 module {
   func.func @main(){
     %c0 = arith.constant 0 : index
-    %cst = arith.constant dense<0.000000e+00> : tensor<1x2x5x5xf64>
-    %cst_0 = arith.constant dense<0.000000e+00> : tensor<4x2x3x3xf64>
+    %cst = arith.constant dense<1.000000e+00> : tensor<1x2x5x5xf64>
+    %cst_0 = arith.constant dense<1.000000e+00> : tensor<4x2x3x3xf64>
     %cst_1 = arith.constant dense<0.000000e+00> : tensor<1x4x3x3xf64>
     %0 = linalg.conv_2d_nchw_fchw ins(%cst, %cst_0 : tensor<1x2x5x5xf64>, tensor<4x2x3x3xf64>) outs(%cst_1 : tensor<1x4x3x3xf64>) -> tensor<1x4x3x3xf64>
     %extracted = tensor.extract %0[%c0, %c0, %c0, %c0] : tensor<1x4x3x3xf64>
